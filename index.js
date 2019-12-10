780458569b3761320a0e0b8e1c135e986b55d4d9
CuubeCon <cubeconlp@gmail.com>
2019-12-10 16:39:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,47 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  /**
+ *
+ *
+ *
+ * $env:debug="true"; node app.js
+ *
+ *
+ */
+
+/**
+ *  Select Debug Mode via env variable
+ */
+let debug = process.env.debug === "true";
+console.log("Test: " + debug);
+
+/**
+ *
+ * TWITCH BOT SECTION
+ *
+ */
+// ES6 syntax
+let TwitchJs = require('twitch-js').default;
+
+// Define configuration options:
+let opts = {
+        identity: {
+            username: 'cubeconlp',
+                password: 'oauth:bhvq4jhuy4ymacztewuarm0drttz91'
+                    },
+        channels: [
+            '#salz0r_tv',
+           '#cubeconlp'
+        ]
+};
+
+const token = 'oauth:bhvq4jhuy4ymacztewuarm0drttz91';
+const username = 'cubeconlp';
+const { chat } = new TwitchJs({ token, username });
+const channel = '#salz0r_tv';
+chat.connect().then(globalUserState => {
+    // Do stuff ...
+    chat.join(channel);
+});
